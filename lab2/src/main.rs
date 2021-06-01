872f9f755d52a576811bf2a452452d814ecaeb84
彭浩然 <i@hurrypeng.cc>
2021-06-02 07:36:41 +0800

    Added pipe support

--- lab2/src/main.rs
+++ lab2/src/main.rs
@@ -2,47 +2,100 @@ use std::io::{stdin, BufRead};
 use std::io::{stdout, Write};
 use std::env;
 use std::process::{exit, Command};
+use std::process::Stdio;
+use std::process::Child;
 
-fn main() -> ! {
-    loop {
+fn main() -> !
+{
+    loop
+    {
         let dir_err = "Getting current dir failed";
         print!("{}# ", env::current_dir().expect(dir_err).to_str().expect(dir_err));
-        stdout().flush();
-
-        let mut cmd = String::new();
-        for line_res in stdin().lock().lines() {
+        stdout().flush().unwrap();
+        
+        let mut input = String::new();
+        for line_res in stdin().lock().lines()
+        {
             let line = line_res.expect("Read a line from stdin failed");
-            cmd = line;
+            input = line;
             break;
         }
-        let mut args = cmd.split_whitespace();
-        let prog = args.next();
 
-        match prog {
-            None => panic!("Not program input"),
-            Some(prog) => {
-                match prog {
-                    "cd" => {
-                        let dir = args.next().expect("No enough args to set current dir");
-                        env::set_current_dir(dir).expect("Changing current dir failed");
+        let mut cmds = input.trim().split(" | ").peekable();
+        let mut prev_cmd = None;
+
+        while let Some(cmd) = cmds.next()
+        {
+            let mut args = cmd.split_whitespace();
+            let prog = args.next().unwrap();
+
+            match prog
+            {
+                "cd" =>
+                {
+                    let dir = args.next().expect("No enough args to set current dir");
+                    env::set_current_dir(dir).expect("Changing current dir failed");
+                    prev_cmd = None;
+                }
+                "pwd" =>
+                {
+                    println!("{}", env::current_dir().expect(dir_err).to_str().expect(dir_err));
+                    prev_cmd = None;
+                }
+                "export" =>
+                {
+                    for arg in args {
+                        let mut assign = arg.split("=");
+                        let name = assign.next().expect("No variable name");
+                        let value = assign.next().expect("No variable value");
+                        env::set_var(name, value);
                     }
-                    "pwd" => {
-                        let err = "Getting current dir failed";
-                        println!("{}", env::current_dir().expect(err).to_str().expect(err));
-                    }
-                    "export" => {
-                        for arg in args {
-                            let mut assign = arg.split("=");
-                            let name = assign.next().expect("No variable name");
-                            let value = assign.next().expect("No variable value");
-                            env::set_var(name, value);
-                        }
-                    }
-                    "exit" => {
-                        exit(0);
-                    }
-                    _ => {
-                        Command::new(prog).args(args).status().expect("Run program failed");
+                    prev_cmd = None;
+                }
+                "exit" =>
+                {
+                    exit(0);
+                }
+                prog => {
+                    let stdin = prev_cmd
+                        .map_or(
+                            Stdio::inherit(),
+                            |output: Child| Stdio::from(output.stdout.unwrap())
+                        );
+
+                    let stdout = if cmds.peek().is_some() {
+                        // there is another command piped behind this one
+                        // prepare to send output to the next command
+                        // 在这个命令后还有另一个命令，准备将其输出到下一个命令
+                        Stdio::piped()
+                    } else {
+                        // there are no more commands piped behind this one
+                        // send output to shell stdout
+                        // 在发送输出到 shell 的 stdout 之后，就没有命令要执行了
+                        Stdio::inherit()
+                    };
+                    
+                    let output = Command::new(prog)
+                        .args(args)
+                        .stdin(stdin)
+                        .stdout(stdout)
+                        .spawn();
+                    
+                    //let output = Command::new(prog).args(args);
+                    //let output = Command::new(prog).args(args).status().expect("Run program failed");
+
+                    match output {
+                        Ok(output) => { prev_cmd = Some(output); },
+                        Err(e) => {
+                            prev_cmd = None;
+                            eprintln!("{}", e);
+                        },
+                    };
+
+                    if let Some(ref mut final_cmd) = prev_cmd {
+                        // block until the final command has finished
+                        // 阻塞一直到命令执行完成
+                        final_cmd.wait().unwrap();
                     }
                 }
             }
