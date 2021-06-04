72bae54c762dd20cf4297c28c399afd516c2d3b0
彭浩然 <i@hurrypeng.cc>
2021-06-04 11:16:52 +0800

    Added support for stdin file redirect

--- lab2/src/main.rs
+++ lab2/src/main.rs
@@ -1,14 +1,14 @@
-
 use std::io::{stdin, BufRead};
 use std::io::{stdout, Write};
 use std::env;
 use std::process::{exit, Command};
 use std::process::Stdio;
 use std::process::Child;
+use std::fs::File;
 
 fn main() -> !
 {
-    // Ctrl+C handle: clear current line and start a new line
+    // Ctrl+C handler: clear current line and start a new line
     ctrlc::set_handler(||{
         let dir_err = "Getting current dir failed";
         print!("\n{}# ", env::current_dir().expect(dir_err).to_str().expect(dir_err));
@@ -32,24 +32,19 @@ fn main() -> !
         let mut cmds = input.trim().split(" | ").peekable();
         let mut prev_cmd = None;
 
-        while let Some(cmd) = cmds.next()
+        while let Some(mut cmd) = cmds.next()
         {
             let mut args = cmd.split_whitespace();
-            let prog = args.next();
-            match prog
+            let mut prog = args.next();
+            
+            // Ctrl+D handler: exit
+            if !prog.is_some()
             {
-                // Ctrl+D hanle: exit
-                None =>
-                {
-                    println!("bye!");
-                    exit(0);
-                }
-                Some (_prog) =>
-                {
-                }
+                println!("bye!");
+                exit(0);
             }
 
-            let prog = prog.unwrap();
+            let mut prog = prog.unwrap();
 
             match prog
             {
@@ -59,13 +54,6 @@ fn main() -> !
                     env::set_current_dir(dir).expect("Changing current dir failed");
                     prev_cmd = None;
                 }
-                /*
-                "pwd" =>
-                {
-                    println!("{}", env::current_dir().expect(dir_err).to_str().expect(dir_err));
-                    prev_cmd = None;
-                }
-                */
                 "export" =>
                 {
                     for arg in args {
@@ -81,34 +69,43 @@ fn main() -> !
                     println!("bye!");
                     exit(0);
                 }
-                prog => {
-                    let stdin = prev_cmd
+                mut prog => {
+                    let mut stdin = prev_cmd
                         .map_or(
                             Stdio::inherit(),
                             |output: Child| Stdio::from(output.stdout.unwrap())
                         );
 
-                    let stdout = if cmds.peek().is_some() {
+                    let mut stdout = if cmds.peek().is_some() {
                         // there is another command piped behind this one
                         // prepare to send output to the next command
-                        // 在这个命令后还有另一个命令，准备将其输出到下一个命令
                         Stdio::piped()
                     } else {
                         // there are no more commands piped behind this one
                         // send output to shell stdout
-                        // 在发送输出到 shell 的 stdout 之后，就没有命令要执行了
                         Stdio::inherit()
                     };
+
+                    let file_redirect_split: Vec<&str> = cmd.split(" < ").collect();
+                    //println!("debug: {:?}", cmd);
+                    //println!("debug: {:?}", file_redirect_split);
+                    if file_redirect_split.len() == 2
+                    {
+                        cmd = file_redirect_split[0];
+                        let filename = file_redirect_split[1];
+                        let inputfile = File::open(filename).unwrap();
+                        stdin = Stdio::from(inputfile);
+                    }
+
+                    args = cmd.split_whitespace();
+                    prog = args.next().unwrap();
                     
                     let output = Command::new(prog)
                         .args(args)
                         .stdin(stdin)
                         .stdout(stdout)
                         .spawn();
-                    
-                    //let output = Command::new(prog).args(args);
-                    //let output = Command::new(prog).args(args).status().expect("Run program failed");
-
+                
                     match output {
                         Ok(output) => { prev_cmd = Some(output); },
                         Err(e) => {
@@ -119,7 +116,6 @@ fn main() -> !
 
                     if let Some(ref mut final_cmd) = prev_cmd {
                         // block until the final command has finished
-                        // 阻塞一直到命令执行完成
                         final_cmd.wait().unwrap();
                     }
                 }
